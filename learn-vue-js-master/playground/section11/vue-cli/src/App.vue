19760128b56fab2bb3e01ded0d85e42984c85744
SeongHoon Jo <tjd0544@naver.com>
2023-11-20 10:43:04 +0900

    vue 초급강의

---
+++ learn-vue-js-master/playground/section11/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ str }}
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: { AppHeader },
+  data: function(){
+    return {
+      str: 'Header'
+    }
+  },
+  comments: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'change'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ learn-vue-js-master/playground/section11/vue-cli/src/a.vue
@@ -0,0 +1,28 @@
+<!-- var appHeader = {
+    template: '<div>header</div>',
+    method: {
+        addNum: function(){
+            
+        }
+    }
+} -->
+
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // javascript
+    method: {
+        addNum: function(){
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
